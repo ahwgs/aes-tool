a02c72d3454ddfee0577ca28ff837d51c508c657
ahwgs <ah_wgs@126.com>
2021-11-03 12:55:36 +0800

    feat: 增加复制功能

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,67 @@
         <header>加密</header>
         <div class="itme">
           <n-input
+            clearable
             v-model:value="value1"
             type="textarea"
             placeholder="加密内容"
           />
         </div>
+        <!-- <div class="itme">
+          图片内容：
+          <n-upload
+            action="/"
+            accept=".jpg, .jpeg, .png"
+            max="1"
+            v-model:file-list="fileList"
+            list-type="image-card"
+          />
+        </div> -->
         <div class="itme">
-          <n-input v-model:value="value2" type="text" placeholder="加密秘钥" />
+          <n-input
+            clearable
+            v-model:value="value2"
+            type="text"
+            placeholder="加密秘钥"
+          />
         </div>
         <div class="itme">
           <n-button @click="onCreate" type="primary">生成</n-button>
+          <n-button style="margin-left: 5px" @click="onReset1" type="default"
+            >清空</n-button
+          >
         </div>
-        <div v-if="value5" class="itme">生成内容： {{ value5 }}</div>
+        <div v-if="value5" @click="onCopy(value5)" class="itme copy-text">
+          生成内容(点击复制)： {{ value5 }}
+        </div>
       </div>
       <div class="row">
         <header>解密</header>
         <div class="itme">
           <n-input
+            clearable
             v-model:value="value3"
             type="textarea"
             placeholder="解密内容"
           />
         </div>
         <div class="itme">
-          <n-input v-model:value="value4" type="text" placeholder="解密秘钥" />
+          <n-input
+            clearable
+            v-model:value="value4"
+            type="text"
+            placeholder="解密秘钥"
+          />
         </div>
         <div class="itme">
           <n-button @click="onDec" type="primary">解密</n-button>
+          <n-button style="margin-left: 5px" @click="onReset2" type="default"
+            >清空</n-button
+          >
         </div>
-        <div v-if="value6" class="itme">解密结果： {{ value6 }}</div>
+        <div v-if="value6" @click="onCopy(value6)" class="itme copy-text">
+          解密结果(点击复制)： {{ value6 }}
+        </div>
       </div>
     </div>
     <footer>
@@ -50,7 +82,7 @@
 </template>
 <script>
 import { defineComponent, ref } from "vue";
-import { NInput, NButton } from "naive-ui";
+import { NInput, NButton, NUpload } from "naive-ui";
 import CryptoJS from "crypto-js";
 
 const encrypt = (word, keyStr) => {
@@ -64,7 +96,7 @@ const decrypt = (word, keyStr) => {
 };
 
 export default defineComponent({
-  components: { NInput, NButton },
+  components: { NInput, NButton, NUpload },
   setup() {
     const value1 = ref(null),
       value2 = ref(null),
@@ -72,25 +104,49 @@ export default defineComponent({
       value4 = ref(null),
       value5 = ref(null),
       value6 = ref(null);
+    // const fileListRef = ref([]);
+
+    const onCopy = (str) => {
+      navigator.clipboard.writeText(str).then(() => {
+        alert(`${str} 已复制到粘贴板`);
+      });
+    };
 
     const onCreate = () => {
+      console.log("fileListRef", fileListRef.value);
       if (!value1.value || !value2.value) {
         alert("请输入加密内容或秘钥");
         return;
       }
       const str = encrypt(value1.value, value2.value);
       value5.value = str;
+      onCopy(str);
     };
 
     const onDec = () => {
       if (!value3.value || !value4.value) {
-        alert("请输入加密内容或秘钥");
+        alert("请输入解密内容或秘钥");
         return;
       }
-      const str = decrypt(value3.value, value4.value);
-      value6.value = str;
+      try {
+        const str = decrypt(value3.value, value4.value);
+        value6.value = str;
+      } catch (e) {
+        alert("解密失败");
+      }
     };
 
+    const onReset1 = () => {
+      value1.value = null;
+      value2.value = null;
+      value5.value = null;
+    };
+
+    const onReset2 = () => {
+      value3.value = null;
+      value4.value = null;
+      value6.value = null;
+    };
     return {
       value1,
       value2,
@@ -100,6 +156,10 @@ export default defineComponent({
       value6,
       onCreate,
       onDec,
+      onCopy,
+      onReset2,
+      onReset1,
+      // fileList: fileListRef,
     };
   },
 });
@@ -122,12 +182,13 @@ footer {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 40px;
+  height: 60px;
   background: cyan;
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
+  font-size: 16px;
 }
 .row {
   flex: 1;
@@ -138,6 +199,9 @@ footer {
 }
 .row-container {
   display: flex;
+}
+.copy-text {
+  cursor: pointer;
 }
 @media screen and (max-width: 500px) {
   .row-container {
